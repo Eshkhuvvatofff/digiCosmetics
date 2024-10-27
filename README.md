1aef35ca6fcd007e3a732a2863450ff77fc43323
Eshkhuvvatoff <eshkhuvvatofff@gmail.com>
2024-10-27 13:39:48 +0500

    update

---
+++ README.md
@@ -0,0 +1,59 @@
+[![Netlify Status](https://api.netlify.com/api/v1/badges/d10053c6-0604-48a2-b891-f317f0bd24fa/deploy-status)](https://app.netlify.com/sites/digicosmetics/deploys)
+
+![alt text](https://github.com/khakiiman/digiCosmetics/blob/main/src/assets/inrtoPic.png?raw=true)
+# Cosmetic Store
+Cosmetic Store is ecommerce new-design website for cosmetic products.
+## Description
+This website created by best new technology like:
+- React.js
+- Tailwindcss
+
+and awesome packages like:
++ Headless UI
++ Hero Icons
++ React Icons
++ Netlify
++ React Image Compare
++ React Animated Numbers
++ React Owl Carousel
++ Swiper js
++ Formkit
++ Framer Motion
+
+## Installation
+### Installation of frontend
+Use the package manager [npm](https://www.npmjs.com/) or [yarn](https://yarnpkg.com/) to install dependencies and run the project.
+
+```cmd
+npm install
+npm run dev
+```
+or
+```cmd
+yarn
+yarn dev
+```
+
+Open http://localhost:3000 with your browser to see the result. 
+
+## Learn More
+
+To learn more about React.js, take a look at the following resources:
+
+- [React.js Documentation](https://react.dev/learn) - learn about React.js.
+- [React.js Community](https://react.dev/community) - see React.js Community.
+
+You can check out [the React.js GitHub repository](https://github.com/facebook/react/releases) - your feedback and contributions are welcome!
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first
+to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## Result
+The final step is to see this awesome store for buy cosmetic products!
+Check out link below:
+
+https://digicosmetics.netlify.app
---
+++ src/data/testimonials.js
@@ -0,0 +1,59 @@
+import profile1 from '../assets/profile1.png';
+import profile2 from '../assets/profile2.png';
+import profile3 from '../assets/profile3.png';
+import profile4 from '../assets/profile4.png';
+import profile5 from '../assets/profile5.png';
+import profile6 from '../assets/profile6.png';
+import profile7 from '../assets/profile7.png';
+
+export const TestimonialsData = [
+  {
+    id: 1,
+    image: profile1,
+    comment:
+      ' Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    name: 'Sara Alendon',
+  },
+  {
+    id: 2,
+    image: profile2,
+    comment:
+      ' Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    name: 'Natasha Portman',
+  },
+  {
+    id: 3,
+    image: profile3,
+    comment:
+      ' Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    name: 'Alex Hormonzi',
+  },
+  {
+    id: 4,
+    image: profile4,
+    comment:
+      ' Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    name: 'Natali Alensaddon',
+  },
+  {
+    id: 5,
+    image: profile5,
+    comment:
+      ' Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    name: 'Lili Anderson',
+  },
+  {
+    id: 6,
+    image: profile6,
+    comment:
+      ' Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    name: 'Alexis Ren',
+  },
+  {
+    id: 7,
+    image: profile7,
+    comment:
+      ' Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    name: 'Bobbi Brown',
+  },
+];
